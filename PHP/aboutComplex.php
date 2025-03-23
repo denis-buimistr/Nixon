6cf6524dde2948b8fb85c2c12da0e2e809cf7ff7
denis-buimistr <denis.buimistr@gmail.com>
2025-03-23 23:29:44 +0200

    LAB_4 PHP

---
+++ PHP/aboutComplex.php
@@ -0,0 +1,118 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>О комплексе | NixonHouse</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <header class="header_complex">
+        <div class="header-top">
+            <div class="container">
+                <div class="header_top-row">
+                    <a href="index.html">
+                        <img src="./img/first-screen__logo.svg" alt="NixonHouse">
+                    </a>
+                    <nav class="nav">
+                        <ul class="nav_list">
+                            <li><a href="#">О комплексе</a></li>
+                            <li><a href="/district.html">Район</a></li>
+                            <li><a href="/catalog.html">Каталог квартир</a></li>
+                            <li><a href="/mortgage.html">Ипотека</a></li> 
+                            <li><a href="#">Контакты</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>  
+
+    <div class="container">
+        <h1 class="title-2">Nixon – новый символ Кишинева</h1>
+        
+        <p>Nixon – современный и инновационный жилой комплекс в Кишиневе. Он сочетает в себе комфорт, стиль и передовые технологии, предлагая жильцам великолепные виды на город и его окрестности.</p>
+        
+        <div class="image-gallery">
+            <img src="/img/aboutComplex/firtsPhoto.webp" alt="Вид дома">
+        </div>
+
+        <p>- Один из самых высоких жилых комплексов в городе, Nixon выделяется своим впечатляющим дизайном и эстетикой, делая его символом современной архитектуры.</p>
+
+        <div class="image-gallery">
+            <img src="/img/aboutComplex/2nPhoto.webp" alt="Современные технологии">
+        </div>
+
+        <p>- Использование современных технологий в строительстве снижает энергопотребление, делая проживание более экономичным и экологичным.</p>
+        <p>- Продуманные конструкции обеспечивают повышенную устойчивость здания к природным явлениям, таким как землетрясения и ураганы.</p>
+
+        <div class="image-gallery">
+            <img src="/img/aboutComplex/3rdPhoto.webp" alt="Квартира">
+        </div>
+
+        <p>- Светлые и просторные квартиры созданы для комфортного проживания, предлагая уют и функциональность на каждом квадратном метре.</p>
+        <p>- Расположение в пяти минутах от центра города обеспечивает удобный доступ к ресторанам, магазинам и развлечениям.</p>
+
+        <div class="image-gallery">
+            <img src="/img/aboutComplex/4thPhoto.webp" alt="Инфраструктура">
+        </div>
+
+        <p>- Развитая инфраструктура: в непосредственной близости находятся супермаркет Kaufland, школы, современная больница MedPark и торговый центр Oasis Mall.</p>
+        <p>- Наличие большого парка рядом позволяет наслаждаться прогулками на свежем воздухе и отдыхать от городской суеты.</p>
+
+        <p>- Все эти преимущества делают Nixon идеальным выбором для тех, кто ищет современное, стильное и комфортное жилье в Кишиневе.</p>
+    </div>
+
+    <footer class="footer">
+        <div class="container">
+            <div class="footer_grid">
+                <div class="footer_logo">
+                    <a href="#">
+                        <img src="./img/first-screen__logo.svg" alt="NixonHouse">
+                    </a>
+                </div>
+                <div class="footer_nav">
+                    <nav>
+                        <ul class="footer_nav-list">
+                            <li><a href="/aboutComplex.html">О комплексе</a></li>
+                            <li><a href="/district.html">Район</a></li>
+                            <li><a href="/catalog.html">Каталог квартир</a></li>
+                            <li><a href="/mortgage.html">Ипотека</a></li> 
+                            <li><a href="#">Контакты</a></li>
+                        </ul>
+                     
+                    </nav>
+                </div>
+                <div class="footer_nav">
+                    <nav>
+                        <ul class="footer_nav-list">
+                            <li><a href="#">Поселение и переезд</a></li>
+                            <li><a href="#">Сервисные услуги</a></li>
+                            <li><a href="#">Экологическая устойчивость</a></li>
+                            <li><a href="#">Инвестиционные возможности</a></li> 
+                            <li><a href="#">Программа лояльности </a></li>
+                        </ul>
+                     
+                    </nav>
+                </div>
+                <div class="footer_address" id="footer">
+                    <ul class="footer_nav-list">
+                        <li>Адрес: Улица Спартакус 36</li>
+                        <li>Телефон: <a href="tel:+37368820853">+373 61 11 11 15</a></li>
+                        <li>Отдел продаж: 09:00 - 18:00</li>
+                        <li>E-mail: <a href="#" class="link-bold">buimistr@gmail.com</a></li>
+                    </ul>
+
+                    <ul class="socials">
+                        <li><a href="#"><img src="/img/socials/youtube.svg" alt="youtube"></a></li>
+                        <li><a href="#"><img src="/img/socials/vk.svg" alt="vk"></a></li>
+                        <li><a href="#"><img src="/img/socials/facebook.svg" alt="facebook"></a></li>
+                        <li><a href="#"><img src="/img/socials/instagram.svg" alt="instagram"></a></li>
+                    </ul>
+                </div>
+          </div>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ PHP/gallery.php
@@ -0,0 +1,114 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gallery</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    
+    <header class="header_complex">
+        <div class="header-top">
+            <div class="container">
+                <div class="header_top-row">
+                    <a href="index.html">
+                        <img src="./img/first-screen__logo.svg" alt="NixonHouse">
+                    </a>
+                    <div class="header-nav">
+                        <nav class="nav">
+                            <ul class="nav_list">
+                                <li><a href="/aboutComplex.html">О комплексе</a></li>
+                                <li><a href="/district.html">Район</a></li>
+                                <li><a href="/catalog.html">Каталог квартир</a></li>
+                                <li><a href="/mortgage.html">Ипотека</a></li> 
+                                <li><a href="#">Контакты</a></li>
+                            </ul>
+                         
+                        </nav>
+                    </div>
+                        
+                    </nav>
+                </div>
+            </div>
+        </div>
+        </header>
+
+        <div class="container">
+            <div class="slider" id="slider">
+                <img src="./img/slider/1.jpg" alt="">
+                <img src="./img/slider/2.jpg" alt="">
+                <img src="./img/slider/3.webp" alt="">
+                <img src="./img/slider/4.jpg" alt="">
+                <img src="./img/slider/5.webp" alt="">
+                <img src="./img/slider/6.png" alt="">
+                <img src="./img/slider/7.png" alt="">
+                <img src="./img/slider/8.png" alt="">
+                <img src="./img/slider/9.png" alt="">
+            </div>
+    
+            <div class="controls">
+                <button id="btnPrev">Назад</button>
+                <button id="btnNext">Вперед</button>
+            </div>
+
+        </div>
+
+<!-- 478.69 + 851 -->
+
+        <footer class="footer ">
+            <div class="container">
+                <div class="footer_grid">
+                    <div class="footer_logo">
+                        <a href="#">
+                            <img src="./img/first-screen__logo.svg" alt="NixonHouse">
+                        </a>
+                    </div>
+                    <div class="footer_nav">
+                        <nav>
+                            <ul class="footer_nav-list">
+                                <li><a href="/aboutComplex.html">О комплексе</a></li>
+                                <li><a href="/district.html">Район</a></li>
+                                <li><a href="/catalog.html">Каталог квартир</a></li>
+                                <li><a href="/gallery.html">Галерея</a></li> 
+                                <li><a href="#">Контакты</a></li>
+                            </ul>
+                         
+                        </nav>
+                    </div>
+                    <div class="footer_nav">
+                        <nav>
+                            <ul class="footer_nav-list">
+                                <li><a href="#">Поселение и переезд</a></li>
+                                <li><a href="#">Сервисные услуги</a></li>
+                                <li><a href="#">Экологическая устойчивость</a></li>
+                                <li><a href="#">Инвестиционные возможности</a></li> 
+                                <li><a href="#">Программа лояльности </a></li>
+                            </ul>
+                         
+                        </nav>
+                    </div>
+                    <div class="footer_address" id="footer">
+                        <ul class="footer_nav-list">
+                            <li>Адрес: Улица Спартакус 36</li>
+                            <li>Телефон: <a href="tel:+37368820853">+373 61 11 11 15</a></li>
+                            <li>Отдел продаж: 09:00 - 18:00</li>
+                            <li>E-mail: <a href="#" class="link-bold">buimistr@gmail.com</a></li>
+                        </ul>
+    
+                        <ul class="socials">
+                            <li><a href="#"><img src="/img/socials/youtube.svg" alt="youtube"></a></li>
+                            <li><a href="#"><img src="/img/socials/vk.svg" alt="vk"></a></li>
+                            <li><a href="#"><img src="/img/socials/facebook.svg" alt="facebook"></a></li>
+                            <li><a href="#"><img src="/img/socials/instagram.svg" alt="instagram"></a></li>
+                        </ul>
+                    </div>
+              </div>
+            </div>
+        </footer>
+
+        <script src="./js/slider.js"></script>
+
+</body>
+</html>
